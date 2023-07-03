5a2612813dfde43cc3a1ca37ee3ff7005ddc7e6a
AngelMatiasA <angel.apaza010@gmail.com>
2023-07-03 17:22:31 -0300

    leyendo archivos

--- practico8/Program.cs
+++ practico8/Program.cs
@@ -7,127 +7,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-
-
         static void Main(string[] args)
         {
-            int cantidad = 0;
-            string entrada = "";
-            bool valida = true;
-            int opcion = 0;
-            ListaTarea ManejoTareas = new ListaTarea();
+            String rutaArch = "";
+            Console.WriteLine("ingrrese la ruta del archivo "); 
+            rutaArch = Console.ReadLine();
 
-            Console.WriteLine("Ingrese la cantidad de tareas: ");
-            cantidad = Convert.ToInt32(Console.ReadLine());
+            List<string> archivos = Directory.GetFiles(rutaArch).ToList<string>();
 
-            ManejoTareas.PedirTareas(cantidad);
-            ManejoTareas.mostrarTareas(ManejoTareas.ListaPendiente);
-            ManejoTareas.moverTareas();
-            /*elimino las tareas de la lista pendientes que se movieron a realizadas*/
-            foreach (var realizadas in ManejoTareas.ListaRealizadas)
+            Console.WriteLine(" Lista de archivos : "); 
+
+            for (int i = 0; i < archivos.Count; i++)
             {
-                ManejoTareas.eliminarPorId(realizadas.ID);
+                Console.WriteLine($"archivo nro {i} nombre: {Path.GetFileName(archivos[i]) }");
             }
-
-            /*Buscar Tarea*/
-            do
-            {
-
-                do
-                {
-                    Console.WriteLine("desea buscar una tare ingrese 1 para si 2 para no");
-                    entrada = Console.ReadLine();
-                    valida = int.TryParse(entrada, out opcion);
-                    if (!valida || (opcion != 2 && opcion != 1))
-                    {
-                        Console.WriteLine("Opcion no valida, intente nuevamente. ");
-
-                    }
-                } while (!valida || (opcion != 2 && opcion != 1));
-                if (opcion == 1)
-                {
-                    Console.WriteLine("Ingrese la tarea a buscar ");
-
-                    entrada = Console.ReadLine();
-                    foreach (var pendiente in ManejoTareas.ListaPendiente)
-                    {
-                        if (pendiente.Descripcion.Equals(entrada))
-                        {
-                            Console.WriteLine("Tarea Encontrada en lista de pendientes ");
-                            ManejoTareas.mostrarTarea(pendiente);
-                            opcion = 0;
-
-                        }
-
-                    }
-                    if (opcion == 1)
-                    {
-
-                        Console.WriteLine("Tarea no encontrada en lista de pendientes");
-                        foreach (var pendiente in ManejoTareas.ListaRealizadas)
-                        {
-                            if (pendiente.Descripcion.Equals(entrada))
-                            {
-                                Console.WriteLine("Tarea Encontrada en lista de Realizadas ");
-                                ManejoTareas.mostrarTarea(pendiente);
-                                opcion = 0;
-
-                            }
-                          
-                        }
-                        if (opcion == 1)
-                        {
-                              
-                                Console.WriteLine("Tarea no encontrada en lista de Realizadas");
-                           
-                            
-                        }
-
-
-                    }
-
-                }
-                else
-                {
-                    valida = false;
-                }
-
-            } while (valida);
-
-            /*sumario de horas */
-            string ruta = "Sumatoria.txt"; 
-            // int totalHr = 0;
-
-        //      foreach (var tarea in ManejoTareas.ListaRealizadas)
-        // {
-        //     totalHr += tarea.Duracion;
-            
-        // }
-        // StreamWriter escritor = new StreamWriter(ruta);
-        // escritor.WriteLine(" Total de horas trabajadas : ");
-        
-        // escritor.WriteLine($" Total: {totalHr}  ");
-        ManejoTareas.sumatoriaDeHrs(ManejoTareas.ListaRealizadas, ruta);
-         
-
-
-
-
-
-            Console.WriteLine("\n \n ***** \n \n ****** \nmuestro tareas realizadas");
-            ManejoTareas.mostrarTareas(ManejoTareas.ListaRealizadas);
-            Console.WriteLine("\n \n ***** \n \n ****** \nmuestro tareas pendientes");
-
-
-            ManejoTareas.mostrarTareas(ManejoTareas.ListaPendiente);
-
-
-
-
-
-
-
-
 
         }
     }
